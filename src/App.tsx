1b437a002a7ee1b4308e36e80a2d927b2b1b912d
Sizan Lamichhane <sizansmith.lamichhane@gmail.com>
2025-03-02 13:29:25 +0545

    :portal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
 import Home from "./components/ui/home/Home";
-import { Services } from "./components/ui/services/Services";
-import Sidebar from "./components/ui/sidebar/Sidebar";
-import Skills from "./components/ui/Skills/Skills";
-import { VideoProjects } from "./components/ui/gallery/VideoProjects";
-import ContactUs from "./components/ui/contactus/ContactUs";
-import ScrollVideo from "./components/ui/about/ScrollVideo";
 import "./App.css";
 function App() {
   return (
