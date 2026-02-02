25c1eb1865fd9090dc14fdde06cdeb565883ce9a
Sizan Lamichhane <sizansmith.lamichhane@gmail.com>
2026-02-02 16:48:32 +0545

    Fix Router crash: Move BrowserRouter to correct entry file (index.tsx)

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import App2 from "../App2";
 import "./index.css";
 
-import { BrowserRouter } from "react-router-dom";
-
 // Disable console logs in production
 if (process.env.NODE_ENV === "production") {
   console.log = function () { };
@@ -15,8 +13,6 @@ if (process.env.NODE_ENV === "production") {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App2 />
-    </BrowserRouter>
+    <App2 />
   </React.StrictMode>
 );
